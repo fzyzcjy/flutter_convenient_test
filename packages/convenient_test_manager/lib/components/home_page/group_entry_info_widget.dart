ff3b5fe835b0f0af9d34a15cac8ebd4d09557c78
fzyzcjy <ch271828n@outlook.com>
2022-05-04 17:47:35 +0800

    fix: nothing is shown

--- packages/convenient_test_manager/lib/components/home_page/group_entry_info_widget.dart
+++ packages/convenient_test_manager/lib/components/home_page/group_entry_info_widget.dart
@@ -53,7 +53,7 @@ class _GroupInfoWidget extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           if (showHeader) _buildHeader(),
-          if (expanding)
+          if (!showHeader || expanding)
             for (final childGroupEntryId in info.entryIds)
               HomePageGroupEntryInfoWidget(groupEntryId: childGroupEntryId),
         ],
