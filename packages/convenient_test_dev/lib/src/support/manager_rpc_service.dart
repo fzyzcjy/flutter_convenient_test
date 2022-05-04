5a5f15ed8b4aeddc9d4337ff51ebf1ef80c009b3
fzyzcjy <ch271828n@outlook.com>
2022-05-04 19:59:50 +0800

    implement ManagerRpcServiceLocalFile

--- packages/convenient_test_dev/lib/src/support/manager_rpc_service.dart
+++ packages/convenient_test_dev/lib/src/support/manager_rpc_service.dart
@@ -1,7 +1,11 @@
+import 'dart:io';
+
 import 'package:convenient_test_common/convenient_test_common.dart';
 import 'package:convenient_test_dev/src/support/slot.dart';
 import 'package:get_it/get_it.dart';
 import 'package:grpc/grpc.dart';
+import 'package:intl/intl.dart';
+import 'package:path_provider/path_provider.dart';
 
 abstract class ManagerRpcService {
   static const _kTag = 'ManagerRpcService';
@@ -50,18 +54,32 @@ ConvenientTestManagerClient createConvenientTestManagerClientStub({required Stri
 class ManagerRpcServiceLocalFile extends ManagerRpcService {
   static const _kTag = 'RpcServiceLocalFile';
 
+  final Future<String> reportPath = _createReportPath();
+
   @override
   Future<WorkerMode> getWorkerMode() async {
-    TODO;
+    return WorkerMode(
+      integrationTest: WorkerModeIntegrationTest(
+        // run all tests
+        filterNameRegex: '.*',
+      ),
+    );
   }
 
   @override
   Future<void> report(ReportCollection request) async {
-    TODO;
+    await File(await reportPath).writeAsBytes(request.writeToBuffer());
   }
 
   @override
   Future<void> resetManagerCache() async {
     Log.d(_kTag, 'resetManagerCache do nothing');
+  }
+
+  static Future<String> _createReportPath() async {
+    final stem = DateFormat('yyyyMMdd_hhmmss').format(DateTime.now());
+    final path = '${(await getTemporaryDirectory()).path}/ConvenientTest_Report_$stem.bin';
+    Log.i(_kTag, '*** Report data will be written to path: $path ***');
+    return path;
   }
 }
