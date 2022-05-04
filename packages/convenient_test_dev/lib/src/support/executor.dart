0c34e81932a517a280c0855fc7f8d2436413e458
fzyzcjy <ch271828n@outlook.com>
2022-05-04 17:02:24 +0800

    move SuiteInfoConverter

--- packages/convenient_test_dev/lib/src/support/executor.dart
+++ packages/convenient_test_dev/lib/src/support/executor.dart
@@ -1,12 +1,11 @@
 // ignore_for_file: implementation_imports
 
 import 'package:convenient_test_common/convenient_test_common.dart';
+import 'package:convenient_test_dev/src/support/suite_info_converter.dart';
 import 'package:convenient_test_dev/src/third_party/my_test_compat.dart';
 import 'package:get_it/get_it.dart';
 import 'package:test_api/src/backend/declarer.dart';
 import 'package:test_api/src/backend/group.dart';
-import 'package:test_api/src/backend/group_entry.dart';
-import 'package:test_api/src/backend/test.dart';
 
 class ConvenientTestExecutor {
   // static const _kTag = 'ConvenientTestExecutor';
@@ -20,45 +19,7 @@ class ConvenientTestExecutor {
   }
 
   static void _reportTestInfoPack(Group group) {
-    final suiteInfo = _SuiteInfoConverter().convert(group);
+    final suiteInfo = SuiteInfoConverter().convert(group);
     GetIt.I.get<ConvenientTestManagerClient>().reportSuiteInfo(suiteInfo);
-  }
-}
-
-class _SuiteInfoConverter {
-  var _unoccupiedId = 100; // initial id should be >0 to avoid confusion
-  int _nextId() => _unoccupiedId++;
-
-  SuiteInfoProto convert(Group root) {
-    final target = SuiteInfoProto();
-    target.groupId = _convertGroup(root, target, -1);
-    return target;
-  }
-
-  int _convertGroup(Group entry, SuiteInfoProto target, int parentId) {
-    final id = _nextId();
-    target.groups.add(GroupInfoProto(
-      id: id,
-      name: entry.name,
-      parentId: parentId,
-      entryIds: entry.entries.map((child) => _convertGroupEntry(child, target, id)),
-    ));
-    return id;
-  }
-
-  int _convertTest(Test entry, SuiteInfoProto target, int parentId) {
-    final id = _nextId();
-    target.tests.add(TestInfoProto(
-      id: id,
-      name: entry.name,
-      parentId: parentId,
-    ));
-    return id;
-  }
-
-  int _convertGroupEntry(GroupEntry entry, SuiteInfoProto target, int parentId) {
-    if (entry is Group) return _convertGroup(entry, target, parentId);
-    if (entry is Test) return _convertTest(entry, target, parentId);
-    throw Exception('Unknown entry=$entry');
   }
 }
