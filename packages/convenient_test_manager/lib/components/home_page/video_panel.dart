2149459a2f5dcc780d122ac8c63e8fc866547f16
fzyzcjy <ch271828n@outlook.com>
2022-05-05 10:37:03 +0800

    extract secondary panel

---
+++ packages/convenient_test_manager/lib/components/home_page/video_panel.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomePageVideoPanel extends StatelessWidget {
+  const HomePageVideoPanel({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TODO;
+  }
+}
