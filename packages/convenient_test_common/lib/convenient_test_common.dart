7fbe4abd6eb989e0b5b03eeabeb9183ac6c65800
fzyzcjy <ch271828n@outlook.com>
2022-05-05 09:04:35 +0800

    add button to misc dialog

--- packages/convenient_test_common/lib/convenient_test_common.dart
+++ packages/convenient_test_common/lib/convenient_test_common.dart
@@ -1,7 +1,9 @@
 export 'package:convenient_test_common/src/common_flutter/collections.dart';
 export 'package:convenient_test_common/src/common_flutter/front_log.dart';
+export 'package:convenient_test_common/src/common_flutter/get_set.dart';
 export 'package:convenient_test_common/src/common_flutter/relations.dart';
 export 'package:convenient_test_common/src/common_flutter/static_section_list_view.dart';
+export 'package:convenient_test_common/src/common_flutter/sync_text_field.dart';
 export 'package:convenient_test_common/src/consts.dart';
 export 'package:convenient_test_common/src/protobuf/convenient_test.pb.dart';
 export 'package:convenient_test_common/src/protobuf/convenient_test.pbgrpc.dart';
