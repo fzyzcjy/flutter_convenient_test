dcd3123d0573058ac6aae6aa9bf95cbf0e139aa0
fzyzcjy <ch271828n@outlook.com>
2022-05-05 15:20:25 +0800

    setupMakeHighlightLogEntryVisible

--- packages/convenient_test_manager/lib/stores/highlight_store.dart
+++ packages/convenient_test_manager/lib/stores/highlight_store.dart
@@ -1,8 +1,12 @@
+import 'dart:math';
+
 import 'package:collection/collection.dart';
 import 'package:convenient_test_common/convenient_test_common.dart';
+import 'package:convenient_test_manager/stores/home_page_store.dart';
 import 'package:convenient_test_manager/stores/log_store.dart';
 import 'package:convenient_test_manager/stores/suite_info_store.dart';
 import 'package:convenient_test_manager/stores/video_store.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:get_it/get_it.dart';
 import 'package:mobx/mobx.dart';
 
@@ -51,6 +55,7 @@ abstract class _HighlightStore with Store {
 
   _HighlightStore() {
     _setupSyncVideoPositionToHighlight();
+    _setupMakeHighlightLogEntryVisible();
   }
 
   void _setupSyncVideoPositionToHighlight() {
@@ -64,7 +69,7 @@ abstract class _HighlightStore with Store {
 
   void _handlePlayerPositionCorrespondingLogEntryIdChange(int? logEntryId) {
     final logStore = GetIt.I.get<LogStore>();
-   
+
     if (logEntryId == null) return;
     Log.d(_kTag, 'update highlight since playerPositionCorrespondingLogEntryId=$logEntryId');
 
@@ -72,6 +77,39 @@ abstract class _HighlightStore with Store {
     expandSeriesForTest(testInfoId: testEntryId);
     highlightTestEntryId = testEntryId;
     highlightLogEntryId = logEntryId;
+  }
+
+  void _setupMakeHighlightLogEntryVisible() {
+    reaction<int?>(
+      (_) => highlightLogEntryId,
+      _handleHighlightLogEntryIdChange,
+    );
+  }
+
+  void _handleHighlightLogEntryIdChange(int? highlightLogEntryId) {
+    final homePageStore = GetIt.I.get<HomePageStore>();
+
+    if (highlightLogEntryId == null) return;
+
+    final listViewIndexForHighlight = _calcListViewIndexForLogEntry(logEntryId: highlightLogEntryId);
+    if (listViewIndexForHighlight == null) return;
+
+    final itemPositions = homePageStore.itemPositionsListener.itemPositions.value.map((e) => e.index).toList();
+    final visibleMaxIndex = itemPositions.fold(0, max);
+    final visibleMinIndex = itemPositions.fold(2147483647, min);
+
+    if (visibleMaxIndex < listViewIndexForHighlight) {
+      Log.d(_kTag, 'scroll DOWN to make index=$listViewIndexForHighlight visible');
+      homePageStore.itemScrollController.jumpTo(index: listViewIndexForHighlight, alignment: .9);
+    }
+    if (visibleMinIndex > listViewIndexForHighlight) {
+      Log.d(_kTag, 'scroll UP to make index=$listViewIndexForHighlight visible');
+      homePageStore.itemScrollController.jumpTo(index: listViewIndexForHighlight, alignment: .0);
+    }
+  }
+
+  int? _calcListViewIndexForLogEntry({required int logEntryId}) {
+    return TODO;
   }
 
   final _logStore = GetIt.I.get<LogStore>();
