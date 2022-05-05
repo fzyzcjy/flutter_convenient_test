522277fbf65721c74b54bdb017a5bfec850a3a2c
fzyzcjy <ch271828n@outlook.com>
2022-05-06 07:53:35 +0800

    add a fruit list in page

--- packages/convenient_test/example/lib/home_page.dart
+++ packages/convenient_test/example/lib/home_page.dart
@@ -1,4 +1,4 @@
-import 'package:convenient_test/convenient_test.dart';
+import 'package:convenient_test_example/fruit_store.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,48 +9,82 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  var count = 0;
-  var starredIndex = 0;
+  var fetching = false;
+  List<String>? fruitNames;
+
+  // TODO remove
+  // var count = 0;
+  // var starredIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('HomePage')),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            const Text('I am home page.', style: TextStyle(fontSize: 18)),
-            const SizedBox(height: 4),
-            Text(
-              '' + (count == 0 ? 'No tap' : (count == 1 ? '1 tap' : '$count taps')),
-              style: const TextStyle(fontSize: 18),
-            ),
-            for (var i = 0; i < 3; ++i)
-              Mark(
-                name: HomePageMark.row,
-                child: ListTile(
-                  title: Text('Row ${i + 1}' + (starredIndex == i ? ' [Starred]' : '')),
-                  trailing: IconButton(
-                    onPressed: () => setState(() => starredIndex = i),
-                    icon: Mark(
-                      name: HomePageMark.star,
-                      child: Icon(starredIndex == i ? Icons.star : Icons.star_border),
-                    ),
+      body: fetching
+          ? const Center(
+              child: Text('Fetching now... (Imagine some network latency here)'),
+            )
+          : fruitNames == null
+              ? Center(
+                  child: TextButton(
+                    onPressed: _fetchFruits,
+                    child: const Text('Tap to fetch fruits'),
+                  ),
+                )
+              : ListView.builder(
+                  itemCount: fruitNames!.length,
+                  itemBuilder: (_, i) => ListTile(
+                    title: Text(fruitNames![i]),
                   ),
                 ),
-              ),
-          ],
-        ),
-      ),
-      floatingActionButton: Mark(
-        name: HomePageMark.button,
-        child: FloatingActionButton(
-          onPressed: () => setState(() => count++),
-          child: const Icon(Icons.add),
-        ),
-      ),
+      // TODO remove
+      // body: Center(
+      //   child: Column(
+      //     mainAxisSize: MainAxisSize.min,
+      //     children: [
+      //       const Text('I am home page.', style: TextStyle(fontSize: 18)),
+      //       const SizedBox(height: 4),
+      //       Text(
+      //         '' + (count == 0 ? 'No tap' : (count == 1 ? '1 tap' : '$count taps')),
+      //         style: const TextStyle(fontSize: 18),
+      //       ),
+      //       for (var i = 0; i < 3; ++i)
+      //         Mark(
+      //           name: HomePageMark.row,
+      //           child: ListTile(
+      //             title: Text('Row ${i + 1}' + (starredIndex == i ? ' [Starred]' : '')),
+      //             trailing: IconButton(
+      //               onPressed: () => setState(() => starredIndex = i),
+      //               icon: Mark(
+      //                 name: HomePageMark.star,
+      //                 child: Icon(starredIndex == i ? Icons.star : Icons.star_border),
+      //               ),
+      //             ),
+      //           ),
+      //         ),
+      //     ],
+      //   ),
+      // ),
+      // floatingActionButton: Mark(
+      //   name: HomePageMark.button,
+      //   child: FloatingActionButton(
+      //     onPressed: () => setState(() => count++),
+      //     child: const Icon(Icons.add),
+      //   ),
+      // ),
     );
+  }
+
+  void _fetchFruits() {
+    setState(() {
+      fetching = true;
+    });
+    FruitStore().fetchFruitNamesFromBackendApi().then((fruits) {
+      setState(() {
+        fruitNames = fruits;
+        fetching = false;
+      });
+    });
   }
 }
 
