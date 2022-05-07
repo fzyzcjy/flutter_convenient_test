e6ed6f5b3c6110145e0cf600ad70252436119809
fzyzcjy <ch271828n@outlook.com>
2022-05-07 22:02:57 +0800

    improve the "header panel" since too many items now #127

--- packages/convenient_test_manager/lib/components/home_page/header_panel.dart
+++ packages/convenient_test_manager/lib/components/home_page/header_panel.dart
@@ -1,4 +1,3 @@
-import 'package:convenient_test_manager/components/home_page/misc_dialog.dart';
 import 'package:convenient_test_manager/services/misc_flutter_service.dart';
 import 'package:convenient_test_manager/stores/highlight_store.dart';
 import 'package:convenient_test_manager_dart/services/vm_service_wrapper_service.dart';
@@ -26,39 +25,39 @@ class HomePageHeaderPanel extends StatelessWidget {
               highlightStore.enableAutoExpand = true;
               miscFlutterService.hotRestartAndRunTests(filterNameRegex: '.*');
             },
-            child: const Text('Run All Tests'),
+            child: const Text('Run All'),
           ),
-          const SizedBox(width: 20),
+          const SizedBox(width: 8),
           TextButton(
             onPressed: miscFlutterService.haltWorker,
             child: const Text('Halt'),
           ),
-          const SizedBox(width: 20),
+          const SizedBox(width: 8),
           TextButton(
             onPressed: miscFlutterService.hotRestartAndRunInAppMode,
             child: const Text('Interactive Mode'),
           ),
-          const SizedBox(width: 20),
+          const SizedBox(width: 8),
           TextButton(
             onPressed: miscFlutterService.reloadInfo,
             child: const Text('Reload Info'),
           ),
-          const SizedBox(width: 20),
+          const SizedBox(width: 8),
           TextButton(
             onPressed: GetIt.I.get<VmServiceWrapperService>().connect,
             child: const Text('Reconnect VM'),
           ),
-          const SizedBox(width: 20),
+          const SizedBox(width: 8),
           TextButton(
             onPressed: miscFlutterService.pickFileAndReadReport,
             child: const Text('Open File'),
           ),
           Expanded(child: Container()),
-          TextButton(
-            onPressed: () => showDialog<dynamic>(context: context, builder: (_) => const HomePageMiscDialog()),
-            child: const Text('Misc'),
-          ),
-          const SizedBox(width: 20),
+          // TextButton(
+          //   onPressed: () => showDialog<dynamic>(context: context, builder: (_) => const HomePageMiscDialog()),
+          //   child: const Text('Misc'),
+          // ),
+          // const SizedBox(width: 8),
           const Text(
             'Isolation Mode',
             style: TextStyle(fontSize: 12, height: 1.2),
