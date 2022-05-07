c414d4bfbce2632758495252c02f8b7db02d9a46
fzyzcjy <ch271828n@outlook.com>
2022-05-07 08:54:01 +0800

    add empty bin file

---
+++ packages/convenient_test_manager_dart/bin/convenient_test_manager_dart.dart
@@ -0,0 +1,9 @@
+import 'package:convenient_test_common_dart/convenient_test_common_dart.dart';
+
+const _kTag = 'main';
+
+Future<void> main(List<String> args) async {
+  Log.i(_kTag, 'main start');
+
+  TODO;
+}
