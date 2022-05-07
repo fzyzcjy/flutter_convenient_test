56fe332d32af3db3184226c93aad56b44c14bc58
fzyzcjy <ch271828n@outlook.com>
2022-05-07 19:52:12 +0800

    remove CI mode flag #126 #118

--- packages/convenient_test_dev/lib/src/support/compile_time_config.dart
+++
@@ -1,6 +0,0 @@
-class CompileTimeConfig {
-  // static const _forceCIMode = true; // for debug
-  static const _forceCIMode = false;
-
-  static const kCIMode = _forceCIMode || bool.fromEnvironment('CONVENIENT_TEST_CI_MODE', defaultValue: false);
-}
