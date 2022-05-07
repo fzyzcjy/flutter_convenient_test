5a7513a9f1cdf65a1a34b8d275dfc600e658e522
fzyzcjy <ch271828n@outlook.com>
2022-05-07 12:20:07 +0800

    create WorkerSuperRunController

--- packages/convenient_test_manager_dart/lib/stores/worker_task_store.dart
+++ packages/convenient_test_manager_dart/lib/stores/worker_task_store.dart
@@ -7,27 +7,62 @@ class WorkerTaskStore = _WorkerTaskStore with _$WorkerTaskStore;
 
 abstract class _WorkerTaskStore with Store {
   @observable
-  WorkerRunMode runMode = WorkerRunMode.interactiveApp;
-
-  @observable
-  var filterNameRegex = kRegexMatchNothing;
-
-  WorkerCurrentRunConfig calcCurrentRunConfig() {
-    switch (runMode) {
-      case WorkerRunMode.interactiveApp:
-        return WorkerCurrentRunConfig(interactiveApp: WorkerCurrentRunConfig_InteractiveApp());
-      case WorkerRunMode.integrationTest:
-        return WorkerCurrentRunConfig(
-          integrationTest: WorkerCurrentRunConfig_IntegrationTest(
-            reportSuiteInfo: TODO,
-            executionFilter: ExecutionFilter(
-              filterNameRegex: filterNameRegex,
-              strategy: TODO,
-            ),
-          ),
-        );
-    }
-  }
+  var superRunController = WorkerSuperRunControllerIntegrationTestClassicalMode(filterNameRegex: kRegexMatchNothing);
 }
 
 enum WorkerRunMode { interactiveApp, integrationTest }
+
+/// A "worker run" is the code execution from worker hot-restart to the next hot-restart
+/// A "worker super run" is one or multiple "worker run"s
+abstract class WorkerSuperRunController {
+  const WorkerSuperRunController();
+
+  WorkerCurrentRunConfig calcCurrentRunConfig();
+}
+
+class WorkerSuperRunControllerInteractiveApp extends WorkerSuperRunController {
+  @override
+  WorkerCurrentRunConfig calcCurrentRunConfig() {
+    return WorkerCurrentRunConfig(interactiveApp: WorkerCurrentRunConfig_InteractiveApp());
+  }
+}
+
+/// "classical mode": no hot-restart between running two tests
+class WorkerSuperRunControllerIntegrationTestClassicalMode extends WorkerSuperRunController {
+  final String filterNameRegex;
+
+  WorkerSuperRunControllerIntegrationTestClassicalMode({required this.filterNameRegex});
+
+  @override
+  WorkerCurrentRunConfig calcCurrentRunConfig() {
+    return WorkerCurrentRunConfig(
+      integrationTest: WorkerCurrentRunConfig_IntegrationTest(
+        reportSuiteInfo: true,
+        executionFilter: ExecutionFilter(
+          filterNameRegex: filterNameRegex,
+          strategy: ExecutionFilter_Strategy(allMatch: ExecutionFilter_Strategy_AllMatch()),
+        ),
+      ),
+    );
+  }
+}
+
+/// "isolation mode": *has* hot-restart between running two tests
+class WorkerSuperRunControllerIntegrationTestIsolationMode extends WorkerSuperRunController {
+  final String filterNameRegex;
+
+  WorkerSuperRunControllerIntegrationTestIsolationMode({required this.filterNameRegex});
+
+  @override
+  WorkerCurrentRunConfig calcCurrentRunConfig() {
+    return WorkerCurrentRunConfig(
+      integrationTest: WorkerCurrentRunConfig_IntegrationTest(
+        reportSuiteInfo: TODO,
+        executionFilter: ExecutionFilter(
+          filterNameRegex: filterNameRegex,
+          strategy: TODO,
+        ),
+      ),
+    );
+  }
+}
--- packages/convenient_test_manager_dart/lib/stores/worker_task_store.g.dart
+++ packages/convenient_test_manager_dart/lib/stores/worker_task_store.g.dart
@@ -9,43 +9,27 @@ part of 'worker_task_store.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
 
 mixin _$WorkerTaskStore on _WorkerTaskStore, Store {
-  late final _$runModeAtom =
-      Atom(name: '_WorkerTaskStore.runMode', context: context);
+  late final _$superRunControllerAtom =
+      Atom(name: '_WorkerTaskStore.superRunController', context: context);
 
   @override
-  WorkerRunMode get runMode {
-    _$runModeAtom.reportRead();
-    return super.runMode;
+  WorkerSuperRunControllerIntegrationTestClassicalMode get superRunController {
+    _$superRunControllerAtom.reportRead();
+    return super.superRunController;
   }
 
   @override
-  set runMode(WorkerRunMode value) {
-    _$runModeAtom.reportWrite(value, super.runMode, () {
-      super.runMode = value;
-    });
-  }
-
-  late final _$filterNameRegexAtom =
-      Atom(name: '_WorkerTaskStore.filterNameRegex', context: context);
-
-  @override
-  String get filterNameRegex {
-    _$filterNameRegexAtom.reportRead();
-    return super.filterNameRegex;
-  }
-
-  @override
-  set filterNameRegex(String value) {
-    _$filterNameRegexAtom.reportWrite(value, super.filterNameRegex, () {
-      super.filterNameRegex = value;
+  set superRunController(
+      WorkerSuperRunControllerIntegrationTestClassicalMode value) {
+    _$superRunControllerAtom.reportWrite(value, super.superRunController, () {
+      super.superRunController = value;
     });
   }
 
   @override
   String toString() {
     return '''
-runMode: ${runMode},
-filterNameRegex: ${filterNameRegex}
+superRunController: ${superRunController}
     ''';
   }
 }
