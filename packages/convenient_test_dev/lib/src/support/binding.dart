a5bd41b2e4ec0c5ac2b0c57428d15072f0a155e3
fzyzcjy <ch271828n@outlook.com>
2022-05-08 10:24:51 +0800

    hack MyIntegrationTestWidgetsFlutterBinding.pointerEventSource

--- packages/convenient_test_dev/lib/src/support/binding.dart
+++ packages/convenient_test_dev/lib/src/support/binding.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
 class MyIntegrationTestWidgetsFlutterBinding extends IntegrationTestWidgetsFlutterBinding {
@@ -9,5 +10,15 @@ class MyIntegrationTestWidgetsFlutterBinding extends IntegrationTestWidgetsFlutt
     }
     assert(WidgetsBinding.instance is MyIntegrationTestWidgetsFlutterBinding); // NOTE MODIFIED
     return WidgetsBinding.instance!;
+  }
+
+  // NOTE HACK #138
+  @override
+  TestBindingEventSource get pointerEventSource => TestBindingEventSource.test;
+
+  @override
+  void handlePointerEvent(PointerEvent event) {
+    print('hi handlePointerEvent $event');
+    super.handlePointerEvent(event);
   }
 }
