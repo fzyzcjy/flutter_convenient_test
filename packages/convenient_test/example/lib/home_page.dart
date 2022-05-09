f50d5430313756256c149c8f2fd4bbc4f2112c47
fzyzcjy <ch271828n@outlook.com>
2022-05-09 20:59:36 +0800

    remove unused code

--- packages/convenient_test/example/lib/home_page.dart
+++ packages/convenient_test/example/lib/home_page.dart
@@ -14,10 +14,6 @@ class _HomePageState extends State<HomePage> {
   List<String>? fruits;
   final chosenFruits = <String>[];
 
-  // TODO remove
-  // var count = 0;
-  // var starredIndex = 0;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,34 +26,6 @@ class _HomePageState extends State<HomePage> {
           child: const Icon(Icons.done),
         ),
       ),
-      // TODO remove
-      // body: Center(
-      //   child: Column(
-      //     mainAxisSize: MainAxisSize.min,
-      //     children: [
-      //       const Text('I am home page.', style: TextStyle(fontSize: 18)),
-      //       const SizedBox(height: 4),
-      //       Text(
-      //         '' + (count == 0 ? 'No tap' : (count == 1 ? '1 tap' : '$count taps')),
-      //         style: const TextStyle(fontSize: 18),
-      //       ),
-      //       for (var i = 0; i < 3; ++i)
-      //         Mark(
-      //           name: HomePageMark.row,
-      //           child: ListTile(
-      //             title: Text('Row ${i + 1}' + (starredIndex == i ? ' [Starred]' : '')),
-      //             trailing: IconButton(
-      //               onPressed: () => setState(() => starredIndex = i),
-      //               icon: Mark(
-      //                 name: HomePageMark.star,
-      //                 child: Icon(starredIndex == i ? Icons.star : Icons.star_border),
-      //               ),
-      //             ),
-      //           ),
-      //         ),
-      //     ],
-      //   ),
-      // ),
     );
   }
 
