ace8aa58893b610eb62da7c16fd711079130c52d
fzyzcjy <ch271828n@outlook.com>
2022-05-09 21:13:03 +0800

    add "chosen point"

--- packages/convenient_test/example/lib/zoom_page.dart
+++ packages/convenient_test/example/lib/zoom_page.dart
@@ -2,45 +2,65 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 
-class ZoomPage extends StatelessWidget {
+class ZoomPage extends StatefulWidget {
   const ZoomPage({Key? key}) : super(key: key);
+
+  @override
+  State<ZoomPage> createState() => _ZoomPageState();
+}
+
+class _ZoomPageState extends State<ZoomPage> {
+  static const _colorArr = Colors.primaries;
+  static const _shadeArr = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900];
+
+  var _chosenPoint = const Point(0, 0);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('ZoomPage')),
       body: Center(
-        child: SizedBox(
-          width: 275,
-          height: 375,
-          child: InteractiveViewer(
-            constrained: false,
-            child: Column(
-              children: [
-                for (var y = 0; y < _colorArr.length; ++y)
-                  Row(
-                    children: [
-                      for (var x = 0; x < _shadeArr.length; ++x)
-                        Container(
-                          width: 50,
-                          height: 50,
-                          color: _getColor(Point(x, y)),
-                          child: Center(
-                            child: Text('$x#$y'),
-                          ),
-                        )
-                    ],
-                  )
-              ],
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text('ChosenPoint: ${_chosenPoint.x}#${_chosenPoint.y}'),
+            SizedBox(
+              width: 275,
+              height: 375,
+              child: InteractiveViewer(
+                constrained: false,
+                child: Column(
+                  children: [
+                    for (var y = 0; y < _colorArr.length; ++y)
+                      Row(
+                        children: [
+                          for (var x = 0; x < _shadeArr.length; ++x)
+                            InkWell(
+                              onTap: () {
+                                setState(() {
+                                  _chosenPoint = Point(x, y);
+                                });
+                              },
+                              child: Container(
+                                width: 50,
+                                height: 50,
+                                color: _getColor(Point(x, y)),
+                                child: Center(
+                                  child: Text('$x#$y'),
+                                ),
+                              ),
+                            )
+                        ],
+                      )
+                  ],
+                ),
+              ),
             ),
-          ),
+          ],
         ),
       ),
     );
   }
-
-  static const _colorArr = Colors.primaries;
-  static const _shadeArr = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900];
 
   Color _getColor(Point<int> point) => _colorArr[point.y][_shadeArr[point.x]]!;
 }
