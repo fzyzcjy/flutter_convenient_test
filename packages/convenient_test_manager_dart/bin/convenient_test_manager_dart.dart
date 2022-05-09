4af0038b9312aaccfaedfbd666d2b604e756f649
fzyzcjy <ch271828n@outlook.com>
2022-05-09 18:38:32 +0800

    in ci mode, when manager has started but worker has not yet started, should gracefully handle this case #165

--- packages/convenient_test_manager_dart/bin/convenient_test_manager_dart.dart
+++ packages/convenient_test_manager_dart/bin/convenient_test_manager_dart.dart
@@ -7,7 +7,6 @@ import 'package:convenient_test_manager_dart/services/misc_dart_service.dart';
 import 'package:convenient_test_manager_dart/services/status_periodic_logger.dart';
 import 'package:convenient_test_manager_dart/services/vm_service_wrapper_service.dart';
 import 'package:get_it/get_it.dart';
-import 'package:mobx/mobx.dart';
 
 const _kTag = 'main';
 
@@ -19,8 +18,32 @@ Future<void> main(List<String> args) async {
 
   setup();
 
-  await asyncWhen((_) => GetIt.I.get<VmServiceWrapperService>().hotRestartAvailable);
+  Log.i(_kTag, 'step awaitWorkerAvailable');
+  await _awaitWorkerAvailable();
+
+  Log.i(_kTag, 'step hotRestartAndRunTests');
   unawaited(GetIt.I.get<MiscDartService>().hotRestartAndRunTests(filterNameRegex: RegexUtils.kMatchEverything));
 
   StatusPeriodicLogger.run();
+}
+
+Future<void> _awaitWorkerAvailable() async {
+  final vmServiceWrapperService = GetIt.I.get<VmServiceWrapperService>();
+
+  Log.i(_kTag, 'waitWorkerAvailable start');
+
+  while (true) {
+    Log.i(_kTag, 'waitWorkerAvailable check');
+    if (vmServiceWrapperService.hotRestartAvailable) {
+      return;
+    }
+
+    // for example, manager has started + worker has not started. Then the initial `connect` will immediately fail.
+    // without this re-connect, it will forever non-connected
+    if (!vmServiceWrapperService.connected) {
+      await vmServiceWrapperService.connect();
+    }
+
+    await Future<void>.delayed(const Duration(seconds: 3));
+  }
 }
