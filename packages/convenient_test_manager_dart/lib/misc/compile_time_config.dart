99e52577a27d38b876ad32a4ce50bf0be31e396a
fzyzcjy <ch271828n@outlook.com>
2022-05-09 14:41:08 +0800

    add CONVENIENT_TEST_SAVE_REPORT

--- packages/convenient_test_manager_dart/lib/misc/compile_time_config.dart
+++ packages/convenient_test_manager_dart/lib/misc/compile_time_config.dart
@@ -1,4 +1,5 @@
 class CompileTimeConfig {
   static const kDefaultEnableIsolationMode =
       bool.fromEnvironment('CONVENIENT_TEST_ISOLATION_MODE', defaultValue: false);
+  static const kSaveReport = bool.fromEnvironment('CONVENIENT_TEST_SAVE_REPORT', defaultValue: false);
 }
