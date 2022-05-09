b56d996eb8b92fe2ee789e7c78abb7817c0cb022
fzyzcjy <ch271828n@outlook.com>
2022-05-09 21:01:54 +0800

    add link to ZoomPage

--- packages/convenient_test/example/lib/second_page.dart
+++ packages/convenient_test/example/lib/second_page.dart
@@ -10,10 +10,28 @@ class SecondPage extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(title: const Text('SecondPage')),
-      body: Center(
-        child: Text(
-          'See fruits: ${chosenFruits.join(', ')}',
-          style: const TextStyle(fontSize: 18, height: 1.5),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 24),
+        child: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              child: Text(
+                'See fruits: ${chosenFruits.join(', ')}',
+                style: const TextStyle(fontSize: 18, height: 1.5),
+              ),
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextButton(onPressed: () => Navigator.pushNamed(context, '/timer'), child: const Text('GoTimerPage')),
+                TextButton(onPressed: () => Navigator.pushNamed(context, '/zoom'), child: const Text('GoZoomPage')),
+              ],
+            ),
+          ],
         ),
       ),
     );
