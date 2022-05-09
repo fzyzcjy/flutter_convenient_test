a400ea2152338509e8890aaa456cbc774a1ead82
fzyzcjy <ch271828n@outlook.com>
2022-05-09 14:54:53 +0800

    run all if started from command line

--- packages/convenient_test_manager_dart/bin/convenient_test_manager_dart.dart
+++ packages/convenient_test_manager_dart/bin/convenient_test_manager_dart.dart
@@ -1,10 +1,15 @@
+import 'dart:async';
+
 import 'package:convenient_test_common_dart/convenient_test_common_dart.dart';
 import 'package:convenient_test_manager_dart/misc/setup.dart';
+import 'package:convenient_test_manager_dart/services/misc_dart_service.dart';
+import 'package:get_it/get_it.dart';
 
 const _kTag = 'main';
 
 Future<void> main(List<String> args) async {
   Log.i(_kTag, 'main start');
   setup();
-  // TODO do we need a infinite loop? or is `setup`'s grpc serve enough?
+
+  unawaited(GetIt.I.get<MiscDartService>().hotRestartAndRunTests(filterNameRegex: RegexUtils.kMatchEverything));
 }
