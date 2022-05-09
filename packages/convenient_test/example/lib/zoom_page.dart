4dbcd5213dcee6c8574c6a1ea9eb13aa7453237c
fzyzcjy <ch271828n@outlook.com>
2022-05-09 21:08:36 +0800

    add ZoomPage content

--- packages/convenient_test/example/lib/zoom_page.dart
+++ packages/convenient_test/example/lib/zoom_page.dart
@@ -7,7 +7,33 @@ class ZoomPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('ZoomPage')),
-      body: Text('TODO'),
+      body: Center(
+        child: SizedBox(
+          width: 275,
+          height: 375,
+          child: InteractiveViewer(
+            constrained: false,
+            child: Column(
+              children: [
+                for (var colorIndex = 0; colorIndex < Colors.primaries.length; ++colorIndex)
+                  Row(
+                    children: [
+                      for (final shade in [50, 100, 200, 300, 400, 500, 600, 700, 800, 900])
+                        Container(
+                          width: 50,
+                          height: 50,
+                          color: Colors.primaries[colorIndex][shade],
+                          child: Center(
+                            child: Text('$colorIndex#${shade ~/ 100}'),
+                          ),
+                        )
+                    ],
+                  )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
