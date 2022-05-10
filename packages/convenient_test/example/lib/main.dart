d48471981fb5d6d9fcb4d9eb0f805cedc3019c39
fzyzcjy <ch271828n@outlook.com>
2022-05-10 11:17:04 +0800

    add TextFieldPage

--- packages/convenient_test/example/lib/main.dart
+++ packages/convenient_test/example/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:convenient_test/convenient_test.dart';
 import 'package:convenient_test_example/home_page.dart';
 import 'package:convenient_test_example/second_page.dart';
+import 'package:convenient_test_example/text_field_page.dart';
 import 'package:convenient_test_example/timer_page.dart';
 import 'package:convenient_test_example/zoom_page.dart';
 import 'package:flutter/material.dart';
@@ -26,6 +27,7 @@ class MyApp extends StatelessWidget {
           '/second': (_) => const SecondPage(),
           '/timer': (_) => const TimerPage(),
           '/zoom': (_) => const ZoomPage(),
+          '/text_field': (_) => const TextFieldPage(),
         },
       ),
     );
---
+++ packages/convenient_test/example/lib/text_field_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class TextFieldPage extends StatefulWidget {
+  const TextFieldPage({Key? key}) : super(key: key);
+
+  @override
+  State<TextFieldPage> createState() => _TextFieldPageState();
+}
+
+class _TextFieldPageState extends State<TextFieldPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('TextFieldPage')),
+      body: const Center(
+        child: TextField(),
+      ),
+    );
+  }
+}
