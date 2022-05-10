fd2702f9fbb59d58f6479c236dbc772fe07f645b
fzyzcjy <ch271828n@outlook.com>
2022-05-10 11:56:03 +0800

    add EnterTextWithoutReplaceLogCallback #175

--- packages/convenient_test_dev/lib/src/functions/widget_tester.dart
+++ packages/convenient_test_dev/lib/src/functions/widget_tester.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+typedef EnterTextWithoutReplaceLogCallback = void Function(TextEditingValue oldValue, TextEditingValue newValue);
+
 extension ExtWidgetTester on WidgetTester {
-  Future<void> enterTextWithoutReplace(Finder finder, String text) async {
+  Future<void> enterTextWithoutReplace(Finder finder, String text,
+      {EnterTextWithoutReplaceLogCallback? logCallback}) async {
     // reference: [enterText]
     await TestAsyncUtils.guard<void>(() async {
       final TextField textField =
@@ -14,6 +17,7 @@ extension ExtWidgetTester on WidgetTester {
 
       final oldValue = controller.value;
       final newValue = _enterTextWithoutReplaceActOnValue(oldValue, text);
+      logCallback?.call(oldValue, newValue);
 
       await showKeyboard(finder);
       testTextInput.updateEditingValue(newValue);
