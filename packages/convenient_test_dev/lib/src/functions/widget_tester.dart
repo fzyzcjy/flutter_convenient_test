f7f2f245564fd44e5e9f0018df8b7a1d2dd26498
fzyzcjy <ch271828n@outlook.com>
2022-05-10 11:50:31 +0800

    try to implement enterTextWithoutReplace #175

--- packages/convenient_test_dev/lib/src/functions/widget_tester.dart
+++ packages/convenient_test_dev/lib/src/functions/widget_tester.dart
@@ -1,7 +1,28 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 extension ExtWidgetTester on WidgetTester {
   Future<void> enterTextWithoutReplace(Finder finder, String text) async {
-    TODO;
+    // reference: [enterText]
+    await TestAsyncUtils.guard<void>(() async {
+      final TextField textField =
+          widget(find.descendant(of: finder, matching: find.byType(TextField), matchRoot: true));
+      final controller = textField.controller;
+      if (controller == null) {
+        throw Exception('To use `enterTextWithoutReplace`, please ensure your TextField has non-null controller');
+      }
+
+      final oldValue = controller.value;
+      final newValue = _enterTextWithoutReplaceActOnValue(oldValue, text);
+
+      await showKeyboard(finder);
+      testTextInput.updateEditingValue(newValue);
+      await idle();
+    });
   }
+}
+
+TextEditingValue _enterTextWithoutReplaceActOnValue(TextEditingValue oldValue, String text) {
+  // TODO ok?
+  return oldValue.replaced(oldValue.selection, text);
 }
