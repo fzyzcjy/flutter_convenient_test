10593dbd3f386596bbd07197bb8fbab14881a0aa
fzyzcjy <ch271828n@outlook.com>
2022-05-10 10:12:55 +0800

    add t.appCodeDir

--- packages/convenient_test_dev/lib/convenient_test_dev.dart
+++ packages/convenient_test_dev/lib/convenient_test_dev.dart
@@ -3,5 +3,6 @@ export 'src/functions/core.dart';
 export 'src/functions/find.dart';
 export 'src/functions/interaction.dart';
 export 'src/functions/log.dart' hide setUpLogTestStartAndEnd;
+export 'src/functions/misc.dart';
 export 'src/functions/widget_controller.dart';
 export 'src/support/slot.dart';
---
+++ packages/convenient_test_dev/lib/src/functions/misc.dart
@@ -0,0 +1,6 @@
+import 'package:convenient_test_dev/convenient_test_dev.dart';
+import 'package:convenient_test_dev/src/support/compile_time_config.dart';
+
+extension ConvenientTestMisc on ConvenientTest {
+  String get appCodeDir => CompileTimeConfig.kAppCodeDir;
+}
