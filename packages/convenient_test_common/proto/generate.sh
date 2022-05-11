a1e4238ebe4bee83b62694fe8227d3bc56f5bd36
fzyzcjy <ch271828n@outlook.com>
2022-05-11 21:57:21 +0800

    remove unused file

--- packages/convenient_test_common/proto/generate.sh
+++
@@ -1,6 +0,0 @@
-set -eux
-
-# NOTE: need to install Dart plugin first. See https://pub.dev/packages/protoc_plugin
-protoc --proto_path=. --dart_out=grpc:../lib/src/protobuf ./convenient_test.proto
-
-flutter format ../lib/src/protobuf --line-length 120
