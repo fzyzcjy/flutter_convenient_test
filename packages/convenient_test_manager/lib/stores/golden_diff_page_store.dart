262de1c44cce4989874db26c0397ad685ffda5c3
fzyzcjy <ch271828n@outlook.com>
2022-05-13 20:12:49 +0800

    add GoldenDiffPageStore

---
+++ packages/convenient_test_manager/lib/stores/golden_diff_page_store.dart
@@ -0,0 +1,9 @@
+import 'package:mobx/mobx.dart';
+
+part 'golden_diff_page_store.g.dart';
+
+class GoldenDiffPageStore = _GoldenDiffPageStore with _$GoldenDiffPageStore;
+
+abstract class _GoldenDiffPageStore with Store {
+  // TODO
+}
