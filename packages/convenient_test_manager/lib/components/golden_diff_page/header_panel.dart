f4232b2fb442953c4fb406c0528ce73cca4de292
fzyzcjy <ch271828n@outlook.com>
2022-05-13 20:22:26 +0800

    display git folder

--- packages/convenient_test_manager/lib/components/golden_diff_page/header_panel.dart
+++ packages/convenient_test_manager/lib/components/golden_diff_page/header_panel.dart
@@ -1,6 +1,7 @@
 import 'package:convenient_test_manager/stores/golden_diff_page_store.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:get_it/get_it.dart';
 
 class GoldenDiffPageHeaderPanel extends StatelessWidget {
@@ -17,6 +18,7 @@ class GoldenDiffPageHeaderPanel extends StatelessWidget {
             onPressed: () => Navigator.pop(context),
             icon: const Icon(Icons.arrow_back),
           ),
+          const SizedBox(width: 24),
           ..._buildGitRepoInfo(),
         ],
       ),
@@ -27,14 +29,25 @@ class GoldenDiffPageHeaderPanel extends StatelessWidget {
     final goldenDiffPageStore = GetIt.I.get<GoldenDiffPageStore>();
 
     return [
+      const Text(
+        'Active Folder: ',
+        style: TextStyle(color: Colors.black87, fontWeight: FontWeight.w600),
+      ),
+      Observer(
+        builder: (_) => Text(
+          goldenDiffPageStore.gitRepo ?? '(Not Selected)',
+          style: const TextStyle(),
+        ),
+      ),
+      const SizedBox(width: 8),
       TextButton(
         onPressed: () async {
           final dirPath = await FilePicker.platform.getDirectoryPath();
           if (dirPath == null) return;
-         
+
           goldenDiffPageStore.gitRepo = dirPath;
         },
-        child: const Text('Open Folder'),
+        child: const Text('Open'),
       ),
     ];
   }
