65f8f820c3ff7c84d9e79463b986c7f62797730e
fzyzcjy <ch271828n@outlook.com>
2022-05-13 21:22:26 +0800

    GoldenDiffPage: allow arrow up/down key to navigate #203

--- packages/convenient_test_manager/lib/components/golden_diff_page/file_info_panel.dart
+++ packages/convenient_test_manager/lib/components/golden_diff_page/file_info_panel.dart
@@ -1,6 +1,7 @@
 import 'package:collection/collection.dart';
 import 'package:convenient_test_manager/stores/golden_diff_page_store.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:get_it/get_it.dart';
 
@@ -19,20 +20,69 @@ class GoldenDiffPageFileInfoPanel extends StatelessWidget {
           gitFolderInfo.diffFileInfos.singleWhereOrNull((info) => info.path == goldenDiffPageStore.highlightPath);
       if (highlightInfo == null) return const Center(child: Text('Please choose an item from left panel'));
 
-      return Row(
-        children: [
-          Expanded(
-            child: Image(
-              image: MemoryImage(highlightInfo.originalContent),
+      return _HotKeyHandlerWidget(
+        onMove: (delta) => _handleMove(gitFolderInfo, delta),
+        child: Row(
+          children: [
+            Expanded(
+              child: Image(
+                image: MemoryImage(highlightInfo.originalContent),
+              ),
             ),
-          ),
-          Expanded(
-            child: Image(
-              image: MemoryImage(highlightInfo.newContent),
+            Expanded(
+              child: Image(
+                image: MemoryImage(highlightInfo.newContent),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       );
     });
   }
+
+  void _handleMove(GitFolderInfo gitFolderInfo, int delta) {
+    final goldenDiffPageStore = GetIt.I.get<GoldenDiffPageStore>();
+
+    final oldIndex = gitFolderInfo.diffFileInfos.indexWhere((info) => info.path == goldenDiffPageStore.highlightPath);
+    if (oldIndex == -1) return;
+
+    final newIndex = (oldIndex + delta + gitFolderInfo.diffFileInfos.length) % gitFolderInfo.diffFileInfos.length;
+    goldenDiffPageStore.highlightPath = gitFolderInfo.diffFileInfos[newIndex].path;
+  }
+}
+
+class _HotKeyHandlerWidget extends StatelessWidget {
+  final void Function(int delta) onMove;
+  final Widget child;
+
+  const _HotKeyHandlerWidget({
+    Key? key,
+    required this.onMove,
+    required this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Shortcuts(
+      shortcuts: <ShortcutActivator, Intent>{
+        LogicalKeySet(LogicalKeyboardKey.arrowUp): const _MoveIntent(-1),
+        LogicalKeySet(LogicalKeyboardKey.arrowDown): const _MoveIntent(1),
+      },
+      child: Actions(
+        actions: {
+          _MoveIntent: CallbackAction<_MoveIntent>(onInvoke: (intent) => onMove(intent.delta)),
+        },
+        child: Focus(
+          autofocus: true,
+          child: child,
+        ),
+      ),
+    );
+  }
+}
+
+class _MoveIntent extends Intent {
+  final int delta;
+
+  const _MoveIntent(this.delta);
 }
