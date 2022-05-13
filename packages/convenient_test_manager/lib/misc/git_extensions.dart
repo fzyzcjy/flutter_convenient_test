ccffb19fde64500555f1ad8fb8c00495b3dc6ec2
fzyzcjy <ch271828n@outlook.com>
2022-05-13 20:30:05 +0800

    implement calcGitFolderInfo

---
+++ packages/convenient_test_manager/lib/misc/git_extensions.dart
@@ -0,0 +1,65 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:git/git.dart';
+
+extension ExtGitDir on GitDir {
+  Future<List<String>> getDiff() async {
+    final pr = await myRunGit(
+      ['diff', '--name-only'],
+      processWorkingDir: path,
+    );
+
+    return (pr.stdout as String).split('\n').where((row) => row.isNotEmpty).toList();
+  }
+
+  Future<List<int>> show({required String ref, required String filePath}) async {
+    final pr = await myRunGit(
+      ['show', '$ref:$filePath'],
+      processWorkingDir: path,
+    );
+
+    return pr.stdout as List<int>;
+  }
+}
+
+// NOTE MODIFIED from [runGit]
+Future<ProcessResult> myRunGit(
+  List<String> args, {
+  bool throwOnError = true,
+  String? processWorkingDir,
+  Encoding? stdoutEncoding = systemEncoding,
+  Encoding? stderrEncoding = systemEncoding,
+}) async {
+  final pr = await Process.run(
+    'git',
+    args,
+    workingDirectory: processWorkingDir,
+    runInShell: true,
+    stdoutEncoding: stdoutEncoding,
+    stderrEncoding: stderrEncoding,
+  );
+
+  if (throwOnError) {
+    _throwIfProcessFailed(pr, 'git', args);
+  }
+  return pr;
+}
+
+void _throwIfProcessFailed(ProcessResult pr, String process, List<String> args) {
+  if (pr.exitCode != 0) {
+    final values = {'Standard out': pr.stdout.toString().trim(), 'Standard error': pr.stderr.toString().trim()}
+      ..removeWhere((k, v) => v.isEmpty);
+
+    String message;
+    if (values.isEmpty) {
+      message = 'Unknown error';
+    } else if (values.length == 1) {
+      message = values.values.single;
+    } else {
+      message = values.entries.map((e) => '${e.key}\n${e.value}').join('\n');
+    }
+
+    throw ProcessException(process, args, message, pr.exitCode);
+  }
+}
--- packages/convenient_test_manager/lib/stores/golden_diff_page_store.dart
+++ packages/convenient_test_manager/lib/stores/golden_diff_page_store.dart
@@ -1,7 +1,12 @@
+import 'dart:io';
+
 import 'package:convenient_test_common/convenient_test_common.dart';
+import 'package:convenient_test_manager/misc/git_extensions.dart';
 import 'package:convenient_test_manager_dart/misc/config.dart';
 import 'package:flutter/foundation.dart';
+import 'package:git/git.dart';
 import 'package:mobx/mobx.dart';
+import 'package:path/path.dart';
 
 part 'golden_diff_page_store.g.dart';
 
@@ -21,13 +26,27 @@ abstract class _GoldenDiffPageStore with Store {
   }
 
   Future<void> _syncGitFolderInfoFromRepo(String? gitRepo) async {
-    if (gitRepo == null) return;
     Log.i(_kTag, 'syncGitInfoFromRepo gitRepo=$gitRepo');
     gitFolderInfo = await _calcGitFolderInfo(gitRepo);
   }
 
-  static Future<GitFolderInfo> _calcGitFolderInfo(String gitRepo) async {
-    return TODO;
+  static Future<GitFolderInfo?> _calcGitFolderInfo(String? gitRepo) async {
+    if (gitRepo == null) return null;
+    if (!await GitDir.isGitDir(gitRepo)) return null;
+
+    final gitDir = await GitDir.fromExisting(gitRepo);
+
+    final diffFilePaths = await gitDir.getDiff();
+
+    final diffFileInfos = await Stream.fromIterable(diffFilePaths).asyncMap((path) async {
+      return GitDiffFileInfo(
+        path: path,
+        originalContent: await gitDir.show(ref: 'HEAD', filePath: path),
+        newContent: await File(join(gitRepo, path)).readAsBytes(),
+      );
+    }).toList();
+
+    return GitFolderInfo(diffFileInfos: diffFileInfos);
   }
 }
 
@@ -43,8 +62,12 @@ class GitFolderInfo {
 @immutable
 class GitDiffFileInfo {
   final String path;
+  final List<int> originalContent;
+  final List<int> newContent;
 
   const GitDiffFileInfo({
     required this.path,
+    required this.originalContent,
+    required this.newContent,
   });
 }
