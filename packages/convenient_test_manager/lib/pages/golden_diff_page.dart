274478b7b6a079aef6008f955584fb2f6038e25e
fzyzcjy <ch271828n@outlook.com>
2022-05-13 21:04:18 +0800

    remove unnecessary Observer

--- packages/convenient_test_manager/lib/pages/golden_diff_page.dart
+++ packages/convenient_test_manager/lib/pages/golden_diff_page.dart
@@ -2,7 +2,6 @@ import 'package:convenient_test_manager/components/golden_diff_page/detail_diff_
 import 'package:convenient_test_manager/components/golden_diff_page/file_info_panel.dart';
 import 'package:convenient_test_manager/components/golden_diff_page/header_panel.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
 
 class GoldenDiffPage extends StatelessWidget {
   const GoldenDiffPage({Key? key}) : super(key: key);
@@ -22,18 +21,16 @@ class _Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Observer(builder: (_) {
-      return Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const GoldenDiffPageHeaderPanel(),
-          Divider(height: 1, thickness: 1, color: Colors.grey[200]),
-          Expanded(
-            child: _buildBody(),
-          ),
-        ],
-      );
-    });
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const GoldenDiffPageHeaderPanel(),
+        Divider(height: 1, thickness: 1, color: Colors.grey[200]),
+        Expanded(
+          child: _buildBody(),
+        ),
+      ],
+    );
   }
 
   Widget _buildBody() {
