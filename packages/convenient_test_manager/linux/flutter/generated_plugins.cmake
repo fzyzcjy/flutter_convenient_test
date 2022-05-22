a8e73010c9beeaf76469a89593872c9da107d683
fzyzcjy <ch271828n@outlook.com>
2022-05-22 21:23:06 +0800

    add window_size dependency #222

--- packages/convenient_test_manager/linux/flutter/generated_plugins.cmake
+++ packages/convenient_test_manager/linux/flutter/generated_plugins.cmake
@@ -4,6 +4,7 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   dart_vlc
+  window_size
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
--- packages/convenient_test_manager/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/convenient_test_manager/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,10 @@ import Foundation
 
 import dart_vlc
 import path_provider_macos
+import window_size
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DartVlcPlugin.register(with: registry.registrar(forPlugin: "DartVlcPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  WindowSizePlugin.register(with: registry.registrar(forPlugin: "WindowSizePlugin"))
 }
