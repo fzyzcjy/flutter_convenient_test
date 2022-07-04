46b0e19174cd86c17f9562fcaef9ddd4ecdd1482
fzyzcjy <ch271828n@outlook.com>
2022-07-04 21:21:58 +0800

    format code

--- packages/convenient_test_dev/lib/src/functions/goldens.g.dart
+++ packages/convenient_test_dev/lib/src/functions/goldens.g.dart
@@ -12,31 +12,24 @@ GoldenConfig _$GoldenConfigFromJson(Map<String, dynamic> json) => GoldenConfig(
       allowUpdate: json['allowUpdate'] as bool,
       maxToleration: json['maxToleration'] == null
           ? null
-          : GoldenTolerationEntry.fromJson(
-              json['maxToleration'] as Map<String, dynamic>),
+          : GoldenTolerationEntry.fromJson(json['maxToleration'] as Map<String, dynamic>),
       greaterThanToleration: (json['greaterThanToleration'] as List<dynamic>?)
-          ?.map(
-              (e) => GoldenTolerationEntry.fromJson(e as Map<String, dynamic>))
+          ?.map((e) => GoldenTolerationEntry.fromJson(e as Map<String, dynamic>))
           .toList(),
     );
 
-Map<String, dynamic> _$GoldenConfigToJson(GoldenConfig instance) =>
-    <String, dynamic>{
+Map<String, dynamic> _$GoldenConfigToJson(GoldenConfig instance) => <String, dynamic>{
       'allowUpdate': instance.allowUpdate,
       'maxToleration': instance.maxToleration,
       'greaterThanToleration': instance.greaterThanToleration,
     };
 
-GoldenTolerationEntry _$GoldenTolerationEntryFromJson(
-        Map<String, dynamic> json) =>
-    GoldenTolerationEntry(
+GoldenTolerationEntry _$GoldenTolerationEntryFromJson(Map<String, dynamic> json) => GoldenTolerationEntry(
       diffPerPixel: json['diffPerPixel'] as int,
       countPercent: (json['countPercent'] as num).toDouble(),
     );
 
-Map<String, dynamic> _$GoldenTolerationEntryToJson(
-        GoldenTolerationEntry instance) =>
-    <String, dynamic>{
+Map<String, dynamic> _$GoldenTolerationEntryToJson(GoldenTolerationEntry instance) => <String, dynamic>{
       'diffPerPixel': instance.diffPerPixel,
       'countPercent': instance.countPercent,
     };
