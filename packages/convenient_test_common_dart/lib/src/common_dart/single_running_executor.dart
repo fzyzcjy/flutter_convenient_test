e970afc6a028f1d4bb64607ddc5ac260f8906aed
fzyzcjy <ch271828n@outlook.com>
2022-07-06 11:19:30 +0800

    import SingleRunningExecutor

---
+++ packages/convenient_test_common_dart/lib/src/common_dart/single_running_executor.dart
@@ -0,0 +1,46 @@
+import 'package:convenient_test_common_dart/src/common_dart/front_log.dart';
+import 'package:meta/meta.dart';
+
+class SingleRunningExecutor<Arg> {
+  static const _kTag = 'SingleRunningExecutor';
+
+  final Future<void> Function(Arg) _runner;
+
+  var _running = false;
+
+  bool get _runAgainAfterCurrentRunFinish => _runAgainAfterCurrentRunFinishPack != null;
+  _TriggerPack<Arg>? _runAgainAfterCurrentRunFinishPack;
+
+  SingleRunningExecutor(this._runner);
+
+  void trigger({required Arg arg, required String reason}) =>
+      _trigger(_TriggerPack(arg: arg, reason: reason, triggerTime: DateTime.now()));
+
+  void _trigger(_TriggerPack<Arg> pack) {
+    if (_running) {
+      _runAgainAfterCurrentRunFinishPack = pack;
+    } else {
+      _runAgainAfterCurrentRunFinishPack = null;
+      _running = true;
+
+      () async {
+        Log.d(_kTag, 'call runner() reason=${pack.reason} triggerTime=${pack.triggerTime} arg=${pack.arg}');
+        try {
+          await _runner(pack.arg);
+        } finally {
+          _running = false;
+          if (_runAgainAfterCurrentRunFinish) _trigger(_runAgainAfterCurrentRunFinishPack!);
+        }
+      }();
+    }
+  }
+}
+
+@immutable
+class _TriggerPack<Arg> {
+  final Arg arg;
+  final String reason;
+  final DateTime triggerTime;
+
+  const _TriggerPack({required this.arg, required this.reason, required this.triggerTime});
+}
