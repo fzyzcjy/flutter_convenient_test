3243463dedfa548b71707719006c87e65b6dbb49
ethan <ethanshumate@gmail.com>
2022-08-18 21:59:32 -0400

    Rename var

--- packages/convenient_test_common_dart/lib/src/consts.dart
+++ packages/convenient_test_common_dart/lib/src/consts.dart
@@ -1,9 +1,10 @@
-const String kConvenientTestManagerHost = String.fromEnvironment('MANAGER_HOST', defaultValue: '127.0.0.1');
+const String kConvenientTestManagerHost =
+    String.fromEnvironment('CONVENIENT_TEST_MANAGER_HOST', defaultValue: '127.0.0.1');
 
-const int kConvenientTestManagerPort = int.fromEnvironment('MANAGER_PORT', defaultValue: 3579);
+const int kConvenientTestManagerPort = int.fromEnvironment('CONVENIENT_TEST_MANAGER_PORT', defaultValue: 3579);
 
-const String kWorkerVmServiceHost = String.fromEnvironment('WORKER_HOST', defaultValue: '127.0.0.1');
+const String kWorkerVmServiceHost = String.fromEnvironment('CONVENIENT_TEST_WORKER_HOST', defaultValue: '127.0.0.1');
 
-const int kWorkerVmServicePort = int.fromEnvironment('WORKER_PORT', defaultValue: 9753);
+const int kWorkerVmServicePort = int.fromEnvironment('CONVENIENT_TEST_WORKER_PORT', defaultValue: 9753);
 
-const String kReportFileExtension = String.fromEnvironment('REPORT_EXT', defaultValue: 'bin');
+const String kReportFileExtension = String.fromEnvironment('CONVENIENT_TEST_REPORT_EXT', defaultValue: 'bin');
