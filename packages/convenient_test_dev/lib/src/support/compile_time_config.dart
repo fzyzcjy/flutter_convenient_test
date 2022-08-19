085364cd4be1ea77cf92f2026199608137f5a447
ethan <ethanshumate@gmail.com>
2022-08-18 20:18:50 -0400

    Allow kConvenientTestManagerHost and kConvenientTestManagerPort to be configured with --dart-define

--- packages/convenient_test_dev/lib/src/support/compile_time_config.dart
+++ packages/convenient_test_dev/lib/src/support/compile_time_config.dart
@@ -1,8 +1,23 @@
+import 'package:convenient_test_common/convenient_test_common.dart';
+
 class CompileTimeConfig {
-  static final kAppCodeDir = () {
-    const key = 'CONVENIENT_TEST_APP_CODE_DIR';
-    const value = String.fromEnvironment(key);
-    if (value.isEmpty) throw Exception('Please provide `$key` via --dart-define');
-    return value;
-  }();
+  static String _getConfigFromEnv(String key, String defaultVal,
+      {bool required = false}) {
+    final value = String.fromEnvironment(key);
+
+    if (value.isEmpty && required) {
+      throw Exception('Please provide `$key` via --dart-define');
+    }
+
+    return value.isEmpty ? defaultVal : value;
+  }
+
+  static String get kAppCodeDir =>
+      _getConfigFromEnv('CONVENIENT_TEST_APP_CODE_DIR', '', required: true);
+
+  static String get dConvenientTestManagerHost => _getConfigFromEnv(
+      'CONVENIENT_TEST_MANAGER_HOST', kConvenientTestManagerHost);
+
+  static int get dConvenientTestManagerPort => int.parse(_getConfigFromEnv(
+      'CONVENIENT_TEST_MANAGER_PORT', kConvenientTestManagerPort.toString()));
 }
--- packages/convenient_test_dev/lib/src/support/manager_rpc_service.dart
+++ packages/convenient_test_dev/lib/src/support/manager_rpc_service.dart
@@ -1,15 +1,18 @@
 import 'package:convenient_test_common/convenient_test_common.dart';
+import 'package:convenient_test_dev/src/support/compile_time_config.dart';
 import 'package:grpc/grpc.dart';
 
 extension ExtConvenientTestManagerClient on ConvenientTestManagerClient {
   static ConvenientTestManagerClient create() {
     final channel = ClientChannel(
-      kConvenientTestManagerHost,
-      port: kConvenientTestManagerPort,
+      CompileTimeConfig.dConvenientTestManagerHost,
+      port: CompileTimeConfig.dConvenientTestManagerPort,
       options: const ChannelOptions(credentials: ChannelCredentials.insecure()),
     );
-    return ConvenientTestManagerClient(channel, options: CallOptions(timeout: null));
+    return ConvenientTestManagerClient(channel,
+        options: CallOptions(timeout: null));
   }
 
-  Future<void> reportSingle(ReportItem item) => report(ReportCollection(items: [item]));
+  Future<void> reportSingle(ReportItem item) =>
+      report(ReportCollection(items: [item]));
 }
