e3fcba88a9ce0edcf88d608a1114839e1b930dda
gilice <104317939+gilice@users.noreply.github.com>
2023-02-19 13:12:55 +0100

    remove test script

--- packages/convenient_test_manager/integration_test/test.dart
+++ packages/convenient_test_manager/integration_test/test.dart
@@ -2,26 +2,30 @@ import 'package:convenient_test_common_dart/convenient_test_common_dart.dart';
 import 'package:convenient_test_manager/main.dart';
 import 'package:convenient_test_manager/misc/setup.dart';
 import 'package:convenient_test_manager_dart/services/convenient_test_manager_service.dart';
+import 'package:convenient_test_manager_dart/services/vm_service_wrapper_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+
+import 'fake_vm_service_wrapper.dart';
 
 var isSetup = false;
 void main() {
   // color scheme breaks goldens, so we need two variants
-  group('UI GOLDENS', () {
-    myAppGoldenTest(ThemeMode.light);
-    myAppGoldenTest(ThemeMode.dark);
-  });
+  myAppGoldenTest(ThemeMode.light);
+  myAppGoldenTest(ThemeMode.dark);
 }
 
 Future<void> myAppGoldenTest(ThemeMode theme) async {
-  testWidgets('UI golden in theme=$theme', (tester) async {
-    if (!isSetup) {
-      await setup();
-      isSetup = true;
-    }
+  testWidgets('UI Golden theme=$theme', (tester) async {
+    await tester.binding.setSurfaceSize(const Size(1920, 1080));
 
-    await tester.pumpWidget(MyApp(theme: theme));
+    if (!isSetup) {
+      await setup(registerVmServiceWrapper: false);
+      getIt.registerSingleton<VmServiceWrapperService>(FakeVmServiceWrapper());
+      isSetup = true;
+			Log.d('myAppGoldenTest', 'setup finished');
+
+    }
 
     await ConvenientTestManagerService().reportInner(ReportCollection(
       items: [
@@ -30,7 +34,7 @@ Future<void> myAppGoldenTest(ThemeMode theme) async {
         )
       ],
     ));
-
+    await tester.pumpWidget(MyApp(theme: theme));
     await expectLater(find.byType(MyApp),
         matchesGoldenFile('manager-golden-${theme.name}.png'));
   });
