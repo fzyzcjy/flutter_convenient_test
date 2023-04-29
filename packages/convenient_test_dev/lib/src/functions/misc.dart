0ca7259a6c9f2610e6b9624ca6d413093930f5ce
fzyzcjy <ch271828n@outlook.com>
2023-04-29 21:03:58 +0800

    chore: fix import error

--- packages/convenient_test_dev/lib/src/functions/misc.dart
+++ packages/convenient_test_dev/lib/src/functions/misc.dart
@@ -1,5 +1,5 @@
 import 'package:convenient_test_dev/convenient_test_dev.dart';
-import 'package:convenient_test_dev/src/support/compile_time_config.dart';
+import 'package:convenient_test_dev/src/support/static_config.dart';
 
 extension ConvenientTestMisc on ConvenientTest {
   String get appCodeDir => StaticConfig.kAppCodeDir;
