d7f948d6c0944fb200131b9aba7fad882ae703ce
fzyzcjy <ch271828n@outlook.com>
2023-04-29 17:56:52 +0800

    mode=widget should configure convenientTestActive=true #8464

--- packages/convenient_test_dev/lib/src/functions/entrypoint_env_widget.dart
+++ packages/convenient_test_dev/lib/src/functions/entrypoint_env_widget.dart
@@ -1,10 +1,13 @@
 import 'dart:ui';
 
+import 'package:convenient_test/convenient_test.dart';
 import 'package:convenient_test_dev/src/functions/log.dart';
 import 'package:meta/meta.dart';
 
 @internal
 Future<void> convenientTestEntrypointWhenEnvWidget(VoidCallback testBody) async {
+  ConvenientTestWrapperWidget.convenientTestActive = true;
+
   setUpLogTestStartAndEnd();
   testBody();
 }
