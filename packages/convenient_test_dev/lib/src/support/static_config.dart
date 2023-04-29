c83b95e65007759391a588e3c8047363dbe2535d
fzyzcjy <ch271828n@outlook.com>
2023-04-29 20:58:25 +0800

    add StaticConfig.kVerbose

--- packages/convenient_test_dev/lib/src/support/static_config.dart
+++ packages/convenient_test_dev/lib/src/support/static_config.dart
@@ -1,8 +1,18 @@
+import 'dart:io';
+
 class StaticConfig {
   static final kAppCodeDir = () {
     const key = 'CONVENIENT_TEST_APP_CODE_DIR';
     const value = String.fromEnvironment(key);
     if (value.isEmpty) throw Exception('Please provide `$key` via --dart-define');
     return value;
+  }();
+
+  static const kVerboseKey = 'CONVENIENT_TEST_VERBOSE';
+
+  static final bool kVerbose = () {
+    const key = kVerboseKey;
+    if (const bool.hasEnvironment(key)) return const bool.fromEnvironment(key);
+    return int.parse(Platform.environment[key] ?? '0') != 0;
   }();
 }
