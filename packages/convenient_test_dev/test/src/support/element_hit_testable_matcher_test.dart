c06fc17f838947cb5502fd928c68add28d77f9fc
fzyzcjy <ch271828n@outlook.com>
2023-04-30 21:10:09 +0800

    fix test #8504

--- packages/convenient_test_dev/test/src/support/element_hit_testable_matcher_test.dart
+++ packages/convenient_test_dev/test/src/support/element_hit_testable_matcher_test.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  WidgetController.hitTestWarningShouldBeFatal = true;
+
   testWidgets('when hit testable, should pass matching', (tester) async {
     await tester
         .pumpWidget(MaterialApp(home: Scaffold(body: IconButton(onPressed: () {}, icon: const Icon(Icons.star)))));
@@ -14,15 +16,21 @@ void main() {
       home: Scaffold(
         body: Stack(
           children: [
-            IconButton(onPressed: () {}, icon: const Icon(Icons.star)),
-            const Positioned.fill(child: Placeholder()),
+            Positioned.fill(child: IconButton(onPressed: () {}, icon: const Icon(Icons.star))),
+            Positioned.fill(child: Container(color: Colors.green)),
           ],
         ),
       ),
     ));
+    // await debugWidgetTestSaveScreenshot();
+
+    // // should fail to tap, since it is not hit testable
+    // await tester.tap(find.byIcon(Icons.star));
+    // await tester.pump();
+
     expect(
       () => expect(find.byIcon(Icons.star), ElementHitTestableMatcher(tester)),
-      throwsA(isA<TestFailure>()),
+      throwsA(isA<TestFailure>().having((e) => e.toString(), 'toString', contains('is not hit-testable'))),
     );
   });
 }
