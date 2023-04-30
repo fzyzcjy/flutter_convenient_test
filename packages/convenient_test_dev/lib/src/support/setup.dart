267f4824074bc681f76e0725f6e114a95b6a2e8f
fzyzcjy <ch271828n@outlook.com>
2023-04-30 14:42:29 +0800

    extract collectIntoDeclarerAndExecute #8489

--- packages/convenient_test_dev/lib/src/support/setup.dart
+++ packages/convenient_test_dev/lib/src/support/setup.dart
@@ -1,9 +1,6 @@
-import 'package:convenient_test_dev/src/support/executor.dart';
-import 'package:convenient_test_dev/src/support/get_it.dart';
 import 'package:convenient_test_dev/src/utils/errors.dart';
 
 Future<void> setup() async {
   setUpConvenientTestExceptionReporter();
   setUpFlutterOnError();
-  myGetIt.registerSingleton<ConvenientTestExecutor>(ConvenientTestExecutor());
 }
