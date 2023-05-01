0161ca3f89c34aea5f9cc22d6178a94d4cf5c171
fzyzcjy <ch271828n@outlook.com>
2023-05-01 18:23:57 +0800

    feat: ExecutionEnv.detect() #8511

--- packages/convenient_test_dev/lib/src/functions/execution_env.dart
+++ packages/convenient_test_dev/lib/src/functions/execution_env.dart
@@ -1,7 +1,17 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
 enum ExecutionEnv {
   /// Run with a simulator / real device, with ConvenientTestManager, etc
   deviceTest,
 
   /// Run without a simulator / real device, just like a standard widget test
-  widgetTest,
+  widgetTest;
+
+  factory ExecutionEnv.detect() {
+    final binding = TestWidgetsFlutterBinding.instance;
+    if (binding is IntegrationTestWidgetsFlutterBinding) return ExecutionEnv.deviceTest;
+    if (binding is AutomatedTestWidgetsFlutterBinding) return ExecutionEnv.widgetTest;
+    throw Exception('unknown binding=${binding.runtimeType}');
+  }
 }
