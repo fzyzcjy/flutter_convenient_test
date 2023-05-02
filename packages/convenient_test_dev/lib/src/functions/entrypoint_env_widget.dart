0a3977469be3eef90f89b6710bc62276d1dab458
fzyzcjy <ch271828n@outlook.com>
2023-05-02 08:27:54 +0800

    when widget test, report by default, since it does not take up resource and it quite helpful #8514

--- packages/convenient_test_dev/lib/src/functions/entrypoint_env_widget.dart
+++ packages/convenient_test_dev/lib/src/functions/entrypoint_env_widget.dart
@@ -1,27 +1,16 @@
-import 'dart:math';
 import 'dart:ui';
 
-import 'package:convenient_test_common_dart/convenient_test_common_dart.dart';
 import 'package:convenient_test_dev/src/functions/log.dart';
 import 'package:convenient_test_dev/src/support/get_it.dart';
 import 'package:convenient_test_dev/src/support/reporter_service.dart';
 import 'package:convenient_test_dev/src/support/spy_declarer.dart';
-import 'package:convenient_test_dev/src/support/static_config.dart';
-import 'package:intl/intl.dart';
 import 'package:meta/meta.dart';
-
-const _kTag = 'entrypoint_env_widget';
 
 @internal
 Future<void> convenientTestEntrypointWhenEnvWidget(VoidCallback testBody) async {
-  final widgetTestReportSaverDirectory = StaticConfig.kWidgetTestReportSaverDirectory;
-  if (widgetTestReportSaverDirectory != null) {
-    final path = '$widgetTestReportSaverDirectory/${_createReportName()}';
-    Log.i(_kTag, 'will save report to path=$path');
-    myGetIt.registerSingleton<WorkerReportSaverService>(WorkerReportSaverServiceSaveToLocal(path: path));
-  } else {
-    // do not register - since we will not report to anywhere
-    Log.i(_kTag, 'will not save any report');
+  final workerReportSaverService = WorkerReportSaverServiceSaveToLocal.create();
+  if (workerReportSaverService != null) {
+    myGetIt.registerSingleton<WorkerReportSaverService>(workerReportSaverService);
   }
 
   setUpLogTestStartAndEnd();
@@ -29,6 +18,3 @@ Future<void> convenientTestEntrypointWhenEnvWidget(VoidCallback testBody) async 
 
   await WorkerReportSaverService.I?.reportSuiteInfo(spyDeclarerGroup);
 }
-
-String _createReportName() =>
-    'WIDGET-TEST-${DateFormat('yyyyMMdd-hhmmss').format(DateTime.now())}-${Random().nextInt(1000).toString().padLeft(3, '0')}.bin';
--- packages/convenient_test_dev/lib/src/support/reporter_service.dart
+++ packages/convenient_test_dev/lib/src/support/reporter_service.dart
@@ -1,10 +1,13 @@
 import 'dart:io';
+import 'dart:math';
 
 import 'package:convenient_test_common_dart/convenient_test_common_dart.dart';
 import 'package:convenient_test_dev/src/support/get_it.dart';
 import 'package:convenient_test_dev/src/support/manager_rpc_service.dart';
 import 'package:convenient_test_dev/src/support/spy_declarer.dart';
+import 'package:convenient_test_dev/src/support/static_config.dart';
 import 'package:convenient_test_dev/src/support/suite_info_converter.dart';
+import 'package:intl/intl.dart';
 
 /// `ManagerReportSaverService` vs `WorkerReportSaverService`: See comments on `ManagerReportSaverService`.
 abstract class WorkerReportSaverService {
@@ -26,9 +29,27 @@ class WorkerReportSaverServiceSendToManager implements WorkerReportSaverService 
 }
 
 class WorkerReportSaverServiceSaveToLocal implements WorkerReportSaverService {
+  static const _kTag = 'WorkerReportSaverServiceSaveToLocal';
+
   final String path;
 
   const WorkerReportSaverServiceSaveToLocal({required this.path});
+
+  static WorkerReportSaverServiceSaveToLocal? create() {
+    final dir = StaticConfig.kWidgetTestReportSaverDirectory ?? '/tmp';
+    if (!Directory(dir).existsSync()) {
+      Log.i(_kTag, 'will not save report since dir=$dir does not exist');
+      return null;
+    }
+
+    final filename =
+        'WIDGET-TEST-${DateFormat('yyyyMMdd-hhmmss').format(DateTime.now())}-${Random().nextInt(1000).toString().padLeft(3, '0')}.bin';
+    final path = '$dir/ConvenientTestWidgetTest/$filename';
+    Log.i(_kTag, 'will save report to path=$path');
+
+    File(path).parent.createSync(recursive: true);
+    return WorkerReportSaverServiceSaveToLocal(path: path);
+  }
 
   @override
   Future<void> report(ReportItem item) async {
