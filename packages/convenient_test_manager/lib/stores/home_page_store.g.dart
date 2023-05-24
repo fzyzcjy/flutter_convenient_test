56aab31ae2d05fe264b6b9d89c6086aa3c43525d
fzyzcjy <ch271828n@outlook.com>
2023-05-24 20:31:17 +0800

    run formatter

--- packages/convenient_test_manager/lib/stores/home_page_store.g.dart
+++ packages/convenient_test_manager/lib/stores/home_page_store.g.dart
@@ -9,8 +9,7 @@ part of 'home_page_store.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
 
 mixin _$HomePageStore on _HomePageStore, Store {
-  late final _$displayLoadedReportModeAtom =
-      Atom(name: '_HomePageStore.displayLoadedReportMode', context: context);
+  late final _$displayLoadedReportModeAtom = Atom(name: '_HomePageStore.displayLoadedReportMode', context: context);
 
   @override
   bool get displayLoadedReportMode {
@@ -20,14 +19,12 @@ mixin _$HomePageStore on _HomePageStore, Store {
 
   @override
   set displayLoadedReportMode(bool value) {
-    _$displayLoadedReportModeAtom
-        .reportWrite(value, super.displayLoadedReportMode, () {
+    _$displayLoadedReportModeAtom.reportWrite(value, super.displayLoadedReportMode, () {
       super.displayLoadedReportMode = value;
     });
   }
 
-  late final _$activeSecondaryPanelTabAtom =
-      Atom(name: '_HomePageStore.activeSecondaryPanelTab', context: context);
+  late final _$activeSecondaryPanelTabAtom = Atom(name: '_HomePageStore.activeSecondaryPanelTab', context: context);
 
   @override
   HomePageSecondaryPanelTab get activeSecondaryPanelTab {
@@ -37,14 +34,12 @@ mixin _$HomePageStore on _HomePageStore, Store {
 
   @override
   set activeSecondaryPanelTab(HomePageSecondaryPanelTab value) {
-    _$activeSecondaryPanelTabAtom
-        .reportWrite(value, super.activeSecondaryPanelTab, () {
+    _$activeSecondaryPanelTabAtom.reportWrite(value, super.activeSecondaryPanelTab, () {
       super.activeSecondaryPanelTab = value;
     });
   }
 
-  late final _$expandSecondaryPanelAtom =
-      Atom(name: '_HomePageStore.expandSecondaryPanel', context: context);
+  late final _$expandSecondaryPanelAtom = Atom(name: '_HomePageStore.expandSecondaryPanel', context: context);
 
   @override
   bool get expandSecondaryPanel {
@@ -54,8 +49,7 @@ mixin _$HomePageStore on _HomePageStore, Store {
 
   @override
   set expandSecondaryPanel(bool value) {
-    _$expandSecondaryPanelAtom.reportWrite(value, super.expandSecondaryPanel,
-        () {
+    _$expandSecondaryPanelAtom.reportWrite(value, super.expandSecondaryPanel, () {
       super.expandSecondaryPanel = value;
     });
   }
